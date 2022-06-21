2fe68afa7b12288f6e2364f02db1e22d306935d5
kwambaimanu@gmail.com <kwambaimanu@gmail.com>
2022-06-21 16:49:45 +0300

    Portfolio

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>CV - KWAMBAI EMMANUEl K</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Kwambai Emmanuel Online CV and Portfolio.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+
+
+	<!-- Font -->
+
+
+
+	<!-- Stylesheets -->
+	<link href="style.css" rel="stylesheet" >
+
+
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5dea5c17d96992700fcb1788/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<div class="heading-wrapper">
+				<div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-location-outline"></i>
+							<div class="right-area">
+								<h5>Eldoret</h5>
+								<h5>Kenya</h5>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-telephone-outline"></i>
+							<div class="right-area">
+								<h5> <a href="tel: + 254708744739"> +254700 523725</a> </h5>
+								<h6>Call, Sms, WhatsApp</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-chatboxes-outline"></i>
+							<div class="right-area">
+								<h5> <a href="mailto:kwambaimanu@gmail.com"> kwambaimanu@gmail.com </a> </h5>
+								<h6>Email me</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+				</div><!-- row -->
+			</div><!-- heading-wrapper -->
+
+			<a class="downlad-btn" target="self" href="images/Kwambai.Emmanuel CV.pdf">Download CV</a>
+		</div><!-- container -->
+	</header>
+
+	<section class="intro-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-1 col-lg-2"></div>
+				<div class="col-md-10 col-lg-8">
+					<div class="intro">
+						<div class="profile-img"><img src="images/hey.jpg" alt=""></div>
+						<h2><b>Kwambai Emmanuel</b></h2>
+						<h4 class="font-yellow">Cloud Engineer</h4>
+						<ul class="information margin-tb-30">
+							<p>Self-motivate, committed individual and a dependable problem solver. My objective is to contribute immensely in the accomplishment of goals with passion, diligence and integrity.A good team player
+and always seeking to gain new learning experience.</p>
+						</ul>
+						<ul class="social-icons">
+							<li><a href="https://github.com/kwambaimanu"><i class="ion-social-github"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/kwambai-emmmanuel/"><i class="ion-social-linkedin"></i></a></li>
+							<li><a href="https://twitter.com/kwambai254"><i class="ion-social-twitter"></i></a></li>
+						</ul>
+					</div><!-- intro -->
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- intro-section -->
+	<hr>
+	<!-- About me -->
+
+	<section class="about-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>About me</b></h3>
+						<h6 class="font-lite-black"><b>TECHNICAL IT SKILLS</b></h6>
+					</div>
+				</div><!-- col-sm-4 -->
+				<div class="col-sm-8">
+					<p class="margin-b-50">• Excellent programming with Python and PHP</p>
+				   <p class="margin-b-50">• Knowledge in MySQL databases </p>
+				      <p class="margin-b-50">• Front development with HTML,CSS and JAVASCRIPT </p>
+				    <p class="margin-b-50"> •  Version control with Git</p>
+				     <p class="margin-b-50"> • AWS cloud technoligies (AWS Certified Solution Architect) </p>
+
+				  <p class="margin-b-50"> •  Troubleshooting and fixing common computer hardware, Windows software and network related problems</p>
+				  <p class="margin-b-50"> •  Proficiency in Microsoft Office suite (Word, Excel, Access, PowerPoint, Publisher) </p>
+				 
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- about-section -->
+		<hr>
+	<section class="experience-section section mt-5" >
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Career Experience</b></h3>
+						<h6 class="font-lite-black"><b>PREVIOUS JOBS</b></h6>
+					</div>
+				</div><!-- col-sm-4 -->
+				<div class="col-sm-8">
+
+					<div class="experience margin-b-50">
+						<h4><b>IT Support Specialist</b></h4>
+						<h5 class="font-yellow"><b>RIVATEX LIMITED</b></h5>
+						<h6 class="margin-t-10"> May 2021 – Aug 2021</h6>
+						<p class="font-semi-white text-white margin-tb-30">I was at Rivatex Limited for a 3-month internship were i was able to get extensive knowledge on:
+							<ul>
+							<li>-System analysis and design: Fixed faulty air conditioning systems, tested 2
+programs and resolved a software issue
+</li>
+							<li>-Networking/LAN & wireless configuration: Worked on configuring routers
+and cabling
+</li>
+							<li>-Database systems-querying the database, collaborated with the database
+administrator solve cases where many users want to access the same
+entries of data.</li>
+							<li>-Financial accounting systems and data management(SERA)
+</li>
+							</ul>
+  </p>
+
+					</div><!-- experience -->
+
+
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+
+	</section><!-- experience-section -->
+
+	<section class="education-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Education</b></h3>
+						<h6 class="font-lite-black"><b>ACADEMIC CAREER</b></h6>
+					</div>
+				</div><!-- col-sm-4 -->
+				<div class="col-sm-8">
+					<div class="education-wrapper">
+						<div class="education margin-b-50">
+							<h4><b>BACHELORS DEGREE IN COMPUTER SCIENCE</b></h4>
+							<h5 class="font-yellow"><b>Moi University</b></h5>
+							<h6 class="font-lite-black margin-t-10">2017 SEPTEMBER TO 2022</h6>
+							<p class="margin-tb-30">Knowledge in  Procedural programming, Object Oriented Programming, Systems hardware and software, Electronics, Statistics, Operating systems, Software engineering, Algorithms, Database systems, Networking, Information systems security, Data structures and Communication skills. </p>
+						</div><!-- education -->
+
+						<div class="education margin-b-50">
+							<h4><b>KENYA CERTIFICATE OF SECONDARY EDUCATION</b></h4>
+							<h5 class="font-yellow"><b>CHEBARA BOYS HIGH SCHOOL</b></h5>
+							<h6 class="font-lite-black margin-t-10">COMPLETED IN NOVEMBER 2016</h6>
+							<p class="margin-tb-30">Scored B+ </p>
+						</div><!-- education -->
+						<div class="education margin-b-50">
+							<h4><b>AWS SOLUTION ARCHITECT CERTIFICATION</b></h4>
+							<h5 class="font-yellow"><b>AZUBI AFRICA</b></h5>
+							<h6 class="font-lite-black margin-t-10">APRIL 2022-AUGUST </h6>
+							
+						</div><!-- education -->
+					</div><!-- education-wrapper -->
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+
+	</section><!-- about-section -->
+	<hr>
+
+	<section class="accordian-section section">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="heading">
+							<h4><b>More</b></h4>
+						</div>
+					</div><!-- col-sm-12 -->
+
+
+					<div class="col-md-12 ">
+						<div class="panel margin-b-30">
+
+
+							<div class="panel-area margin-b-30">
+								<h5 class="panel-title"><b>HOBIES</b>
+									<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+								<p class="panel-desc">
+									•	Browsing<br>
+									•	Learning langauges<br>
+									•	Hiking<br>
+									•	Volunteering<br>
+								</p>
+							</div><!-- panel-area -->
+
+						
+						</div><!-- panel -->
+					</div><!-- col-sm-6-->
+
+				</div><!-- row -->
+			</div><!-- container -->
+		</section><!-- accordian-section -->
+
+	
+
+	<footer>
+		<p class="copyright">
+
+Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+
+		</p>
+
+
+	</footer>
+
+
+	<!-- SCIPTS -->
+
+	<script src="common-js/jquery-3.2.1.min.js"></script>
+
+
+	<script src="common-js/bootstrap.js"></script>
+
+	<script src="common-js/isotope.pkgd.min.js"></script>
+
+	<script src="common-js/jquery.waypoints.min.js"></script>
+
+
+	<script src="common-js/jquery.fluidbox.min.js"></script>
+
+	<script src="common-js/scripts.js"></script>
+
+</body>
+</html>
